2c18ee035557c217cd0d0dc90b9dd93a8fb2b888
siad007 <siad.ardroumli@gmail.com>
2016-02-06 15:12:23 +0100

    More work on services.

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * zf2-featureflags.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @copyright 2016 MehrAlsNix (http://www.mehralsnix.de)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ *
+ * @link      http://github.com/MehrAlsNix/zf2-featureflags
+ */
 
 return [
     'zf_annotation' => [
--- src/FeatureToggle/Factory/ContextFactory.php
+++ src/FeatureToggle/Factory/ContextFactory.php
@@ -1,7 +1,25 @@
 <?php
+/**
+ * zf2-featureflags.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @copyright 2016 MehrAlsNix (http://www.mehralsnix.de)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ *
+ * @link      http://github.com/MehrAlsNix/zf2-featureflags
+ */
 
 namespace MehrAlsNix\FeatureToggle\Factory;
 
+use Qandidate\Toggle\Context;
+use Zend\ServiceManager\Exception\ServiceNotFoundException;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
@@ -11,10 +29,23 @@ class ContextFactory implements FactoryInterface
      * Create service
      *
      * @param ServiceLocatorInterface $serviceLocator
-     * @return mixed
+     *
+     * @return null|Context
+     *
+     * @throws ServiceNotFoundException
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
+        $toggleConfig = (array) $serviceLocator->get('config');
 
+        $factory = $toggleConfig['qandidate_toggle']['context_factory'];
+
+        $context = null;
+
+        if ($factory !== null && $serviceLocator->has($factory)) {
+            $context = $serviceLocator->get($factory);
+        }
+
+        return $context;
     }
 }
